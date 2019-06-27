078a9825ec32ff1a7a0c5bf9a979c08f23a73cec
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-27 23:11:46 +0200

    Added models for comic search result

---
+++ CodingChallenge/CodingChallenge/Model/ComicSearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  ComicSearchResult.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 27/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import Foundation
+
+struct ComicSearchResult {
+    var relevance: Double?
+    var comicSearches: [ComitSearch]?
+}
