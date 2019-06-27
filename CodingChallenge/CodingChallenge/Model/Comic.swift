14f6d0e8b4105dd27f50af4f821e07e76d97d62d
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-27 21:10:33 +0200

    Project setup and folder structure with basic files

---
+++ CodingChallenge/CodingChallenge/Model/Comic.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentComic.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 27/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import Foundation
---
+++ CodingChallenge/CodingChallenge/Networking/ApiUrls.swift
@@ -0,0 +1,15 @@
+//
+//  ApiUrls.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 27/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import Foundation
+
+let current = "http://xkcd.com/info.0.json"
+let spesificComicPartOne = "http://xkcd.com/"
+let spesificComicPathTwo = "/info.0.json"
+let comicSeach = "https://relevantxkcd.appspot.com/process?action=xkcd&query=COMPUTER"
+let comicExplanationUrl = "http://www.explainxkcd.com/2168"
