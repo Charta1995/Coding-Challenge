a23b3af827dded012ff6e3bd403c8c1c298c8a31
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-28 22:10:13 +0200

    Added Protocol file

---
+++ CodingChallenge/CodingChallenge/Protocols.swift
@@ -0,0 +1,12 @@
+//
+//  Protocols.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 28/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import Foundation
+protocol DidTapComicCell {
+    func cellTapped(comic: Comic)
+}
