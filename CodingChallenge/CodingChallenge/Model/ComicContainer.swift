500959d7800bb34beaa80be1c14ee91290701112
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-28 17:05:51 +0200

    Added ComicContainer
    This is to make it possible to save the comic the the cache while keeping the comic as a struct and again to keep it inherit from Decodable

---
+++ CodingChallenge/CodingChallenge/Model/ComicContainer.swift
@@ -0,0 +1,18 @@
+//
+//  ComicContainer.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 28/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import UIKit
+
+class ComicContainer {
+    
+    var comic: Comic!
+    
+    init(comic: Comic) {
+        self.comic = comic
+    }
+}
