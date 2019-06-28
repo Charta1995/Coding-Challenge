3636564db785cc0a2fe84b963b66b5a1816638f9
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-29 01:41:38 +0200

    Prepared comicDetailViewController

--- CodingChallenge/CodingChallenge/Controller/ComicDetails.swift
+++ CodingChallenge/CodingChallenge/Controller/ComicDetails.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ComicDetails: UIViewController {
     
+    @IBOutlet weak var detailInfoTable: UITableView!
+    @IBOutlet weak var imageZoomScrollVie: UIScrollView!
+    @IBOutlet weak var comicImage: UIImageView!
+    
     var selectedComic: Comic?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        detailInfoTable.dataSource = self
+        detailInfoTable.estimatedRowHeight = 100
+        detailInfoTable.rowHeight = UITableView.automaticDimension
+    }
+    
+}
+
+extension ComicDetails: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
 }
---
+++ CodingChallenge/CodingChallenge/View/Cells/TableViewCells/OptionsButtonCell.swift
@@ -0,0 +1,29 @@
+//
+//  OptionsButtonCell.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 29/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import UIKit
+
+class OptionsButtonCell: UITableViewCell {
+    
+    @IBOutlet weak var addToFavoritesBtn: UIButton!
+    @IBOutlet weak var shareBtn: UIButton!
+    
+    func configureCell() {
+        
+    }
+    
+    
+    @IBAction func addToFavorites(_ sender: UIButton!) {
+        
+    }
+    
+    @IBAction func share(_ sender: UIButton!) {
+        
+    }
+    
+}
