d20c07b1721a45c83f9f2a18378bd8903b183fc9
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-28 18:02:04 +0200

    Comic cell
    Added comic, comic image loader and methods to update the ui

--- CodingChallenge/CodingChallenge/View/Cells/CollectionViewCells/ComicCell.swift
+++ CodingChallenge/CodingChallenge/View/Cells/CollectionViewCells/ComicCell.swift
@@ -10,24 +10,72 @@ import UIKit
 
 class ComicCell: UICollectionViewCell {
     
-    @IBOutlet weak var descriptionLbl: UILabel!
     @IBOutlet weak var imageZoomScrollView: UIScrollView!
     @IBOutlet weak var comicImage: UIImageView!
     @IBOutlet weak var publishDate: UILabel!
     @IBOutlet weak var title: UILabel!
     
-    private let decodableWebRequest = DecodableWebRequest()
+    let decodableWebRequest = DecodableWebRequest()
+    let imageLoader = ImageLoader()
+    
+    private var comic: Comic!
     
     func configureCell(row: Int) {
-        
+        loadComic(row: row)
     }
     
     private func loadComic(row: Int) {
+        let comicNumber = row + 1
+        let comicCompleteUrl = "\(decodableWebRequest.spesificComicPartOne)\(comicNumber)\(decodableWebRequest.spesificComicPathTwo)"
         
+        if let theSavedComic = DataService.instance.getComic(url: comicCompleteUrl) {
+            updateComicAndCell(comic: theSavedComic)
+        } else {
+            decodableWebRequest.makeDecodableRequest(decodable: Comic.self, url: comicCompleteUrl, headers: nil, body: nil, httpMethod: .get) { (loadedComic) in
+                if let theLoadedComic = loadedComic {
+                    DataService.instance.setComic(url: comicCompleteUrl, comic: theLoadedComic)
+                    self.updateComicAndCell(comic: theLoadedComic)
+                }
+            }
+        }
+    }
+    
+    private func updateComicAndCell(comic: Comic) {
+        self.comic = comic
+        self.updateUI()
+        self.loadComicImage()
     }
     
     private func loadComicImage() {
-        
+        imageLoader.loadImage(url: comic.img) { (loadedComicImage) in
+            if let theLoadedComicImage = loadedComicImage {
+                self.updateImage(loadedImage: theLoadedComicImage)
+            }
+        }
     }
+    
+    private func updateUI() {
+        updateTitle()
+        updatePublisDate()
+    }
+    
+    private func updateTitle() {
+        if comic.title != "" {
+            self.title.text = comic.title
+        } else if comic.save_title != "" {
+            self.title.text = comic.save_title
+        } else {
+            self.title.text = comic.alt
+        }
+    }
+    
+    private func updatePublisDate() {
+        self.publishDate.text = "\(comic.day)/\(comic.month)/\(comic.year)"
+    }
+    
+    private func updateImage(loadedImage: UIImage) {
+        self.comicImage.image = loadedImage
+    }
+    
     
 }
