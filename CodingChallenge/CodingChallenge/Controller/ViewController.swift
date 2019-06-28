9efd844bc0eb1b22bd910152ff1bb2d72eee5a32
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-28 16:38:38 +0200

    ComicController setup

--- CodingChallenge/CodingChallenge/Controller/ViewController.swift
+++ CodingChallenge/CodingChallenge/Controller/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ComicController: UIViewController {
 
+    @IBOutlet weak var comicCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        comicCollectionView.delegate = self
+        comicCollectionView.dataSource = self
     }
-
 
 }
 
+/*
+    Extentions to make the code more readable and to seperate it.
+ */
+
+extension ComicController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
+extension ComicController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ComicController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize()
+    }
+}
