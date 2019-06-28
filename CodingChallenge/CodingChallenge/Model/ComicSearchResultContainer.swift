579fc592f12108236c0515e32993f9ac68a6ea73
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-28 18:02:44 +0200

    Created new filed

---
+++ CodingChallenge/CodingChallenge/Model/ComicSearchResultContainer.swift
@@ -0,0 +1,19 @@
+//
+//  ComicSearchResultContainer.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 28/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import Foundation
+
+class ComicSearchResultContainer {
+    
+    var comicSearchResult: ComicSearchResult!
+    
+    init(comicSearchResult: ComicSearchResult) {
+        self.comicSearchResult = comicSearchResult
+    }
+    
+}
