a5d30001b67946000027b0a8db8af1bb53fe770f
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-29 20:11:02 +0200

    CoreData manager
    Created core data manager which is responsible for adding, deleting FavoriteComic object to core data. It is also checking if an equalent Comic object is saved or not

---
+++ CodingChallenge/CodingChallenge/CoreData/CoreDataManager.swift
@@ -0,0 +1,81 @@
+//
+//  CoreDataManager.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 29/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+protocol UpdateFavoriteComicDelegate {
+    func update(comics: [FavoriteComic])
+}
+
+class CoreDataManager: NSObject, NSFetchedResultsControllerDelegate {
+    
+    var appDelegate: AppDelegate!
+    var context: NSManagedObjectContext!
+    var fetchRequest: NSFetchRequest<FavoriteComic>!
+    var fetchResultController: NSFetchedResultsController<FavoriteComic>!
+    var updateFavoriteComicDelegate: UpdateFavoriteComicDelegate?
+    
+    override init() {
+        appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+        context = appDelegate.persistentContainer.viewContext
+        fetchRequest = NSFetchRequest<FavoriteComic>.init(entityName: "FavoriteComic")
+        fetchRequest.sortDescriptors = [NSSortDescriptor.init(key: "num", ascending: true)]
+        fetchResultController = NSFetchedResultsController<FavoriteComic>(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    }
+    
+    func checkIfComicIsSaved(comic: Comic) -> Bool {
+        let favoriteComics = try! context.fetch(fetchRequest)
+        for favoriteComic in favoriteComics {
+            if Int(favoriteComic.num) == comic.num {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func addComic(comic: Comic, imageData: Data) {
+        _ = FavoriteComic(result: comic.createDictionaryForSaving(imageData: imageData), managedObjectContext: context)
+        appDelegate.saveContext()
+    }
+    
+    func removeComic(comic: Comic) {
+        let favoriteComics = try! context.fetch(fetchRequest)
+        for favoriteComic in favoriteComics {
+            if Int(favoriteComic.num) == comic.num {
+                context.delete(favoriteComic)
+                appDelegate.saveContext()
+                break
+            }
+        }
+    }
+    
+    func setDelegate(viewControllerListener: UIViewController) {
+        fetchResultController.delegate = self
+        updateFavoriteComicDelegate = viewControllerListener as? UpdateFavoriteComicDelegate
+        do {
+            try fetchResultController.performFetch()
+        } catch let error as NSError {
+            print("Could not load comics: \(error)")
+        } 
+        runUpdateDelegate()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        runUpdateDelegate()
+    }
+    
+    private func runUpdateDelegate() {
+        if let favoriteObjects = fetchResultController.fetchedObjects {
+            updateFavoriteComicDelegate?.update(comics: favoriteObjects)
+        } else {
+            updateFavoriteComicDelegate?.update(comics: [])
+        }
+    }
+    
+}
