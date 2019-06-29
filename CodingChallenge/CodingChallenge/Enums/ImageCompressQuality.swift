a9c15b015e1e4c5f6f1546aa60ae249f0cd141de
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-29 20:08:54 +0200

    Compress quality enum

---
+++ CodingChallenge/CodingChallenge/Enums/ImageCompressQuality.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompressQuality.swift
+//  CodingChallenge
+//
+//  Created by Marius Fagerhol on 29/06/2019.
+//  Copyright © 2019 Marius Fagerhol. All rights reserved.
+//
+
+import UIKit
+
+enum ImageCompressQuality: CGFloat {
+    case lowest = 0
+    case low = 0.25
+    case medium = 0.5
+    case high = 0.75
+    case highest = 1
+}
