d732fa1b8ea02deec662289fe87221f02227f892
ChartaApps <marius.fagerhol@yahoo.com>
2019-06-29 20:08:38 +0200

    Saving
    Created a method who is creating and returning the data which is going to be saved in coredata

--- CodingChallenge/CodingChallenge/Model/Comic.swift
+++ CodingChallenge/CodingChallenge/Model/Comic.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-struct Comic: Decodable {
+public struct Comic: Decodable {
     var month: String
     var num: Int
     var link: String
@@ -38,5 +38,22 @@ struct Comic: Decodable {
         contentToShare.append("Visit explanation: \(comicExplantaion)")
         
         return contentToShare
+    }
+    
+    func createDictionaryForSaving(imageData: Data) -> [String: Any] {
+        var dictionaryForSaving = [String: Any]()
+        dictionaryForSaving["month"] = month
+        dictionaryForSaving["num"] = num
+        dictionaryForSaving["link"] = link
+        dictionaryForSaving["year"] = year
+        dictionaryForSaving["news"] = news
+        dictionaryForSaving["safe_title"] = safe_title
+        dictionaryForSaving["transcript"] = transcript
+        dictionaryForSaving["alt"] = alt
+        dictionaryForSaving["img"] = img
+        dictionaryForSaving["title"] = title
+        dictionaryForSaving["day"] = day
+        dictionaryForSaving["imageData"] = imageData
+        return dictionaryForSaving
     }
 }
